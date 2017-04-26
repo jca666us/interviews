a7f78cdedad7ee6fad11af21e2227ea6d5e732cc
jca666us <arcarese@me.com>
2017-04-26 09:49:26 -0400

    Update Exercise4.java

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -4,6 +4,11 @@ package com.elsevier.education;
 
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
+No.
+
+1. counter should be declared as private static.
+2. The increment, getCount, and resetCount should be calling private static methods, rather than modifying count directly.
+
 */
 public class Exercise4 {
 
